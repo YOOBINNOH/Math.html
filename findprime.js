372b6af093d11dbc84d69e5cea34c552e4152c11
YOOBINNOH <kuninyb@naver.com>
2022-01-26 19:03:46 +0900

    third commit : 각 css, js 생성 , findprime 알고리즘 구현

---
+++ findprime.js
@@ -0,0 +1,16 @@
+function print(){
+
+    var x = document.getElementById("input_number").value
+
+    for(i=2; i<Math.sqrt(x); i++)
+    {{
+        if (x%i == 0){
+            document.write("소수가 아닙니다.");
+            break;      }}
+            
+        document.write("소수 입니다.");
+        break;    
+
+    }
+
+}
